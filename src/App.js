09d19f9df9455d3fcbb0dfeb5c95f1046dcab82b
MukundiwaShe <nicolezonke393@gmail.com>
2025-08-08 11:00:45 +0200

    Add landing page component with styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import './components//LandingPage.css'
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
+     <LandingPage />
       <button>click me</button>
     </div>
   );
